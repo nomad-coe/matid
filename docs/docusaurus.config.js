198096d32fe64e9b89fb13ec636837c0f6feb8de
Lauri Himanen <lauri.himanen@gmail.com>
2023-07-19 20:43:10 +0300

    Updated deployment branch.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/matid',
-  deploymentBranch: 'v2.0.0',
+  deploymentBranch: 'gh-pages',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nomad-coe', // Usually your GitHub org/user name.
