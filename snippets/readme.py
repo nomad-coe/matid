38eb8c351ba9ce6bea16dd32f05c799f1e488534
Lauri Himanen <lauri.himanen@gmail.com>
2023-12-16 23:48:31 +0200

    Deploy website - based on 4d9688138d564c527621ca11bc3dc02d27f492f8

---
+++ snippets/readme.py
@@ -0,0 +1,38 @@
+import ase.io
+from ase.visualize import view
+
+from matid.clustering import SBC
+from matid.symmetry import SymmetryAnalyzer
+
+# Load structure from a file
+system = ase.io.read('data/system.xyz')
+
+# Find interesting substructures using Symmetry-based Clustering (SBC)
+sbc = SBC()
+clusters = sbc.get_clusters(system)
+
+# Analyze each found cluster printing out the indices of the atoms belonging to
+# this cluster and visualizing the conventional cell from which the cluster was
+# built from.
+for cluster in clusters:
+
+    # Get the indices of the atoms belonging to this cluster
+    indices = cluster.indices
+    print(indices)
+
+    # Get the dimensionality of the cluster
+    dimensionality = cluster.get_dimensionality()
+    print(dimensionality)
+
+    # Get the cell from which the cluster is constructed from. The periodicity
+    # of this cell indicates in which directions the unit cell has been found to
+    # be repeated in (at least once, possibly infinitely).
+    cell = cluster.get_cell()
+    n_repeated_directions = sum(cell.get_pbc())
+    print(n_repeated_directions)
+
+    # Analyze some symmetry properties of the underlying cell to better identify
+    # the material from which the cluster has been constructed from.
+    analyzer = SymmetryAnalyzer(cell, symmetry_tol=0.5)
+    conv_sys = analyzer.get_conventional_system()
+    view(conv_sys)
