e7999ae8579bddf833a5fa8a7450a64ff65d6964
Lauri Himanen <lauri.himanen@gmail.com>
2024-02-26 20:40:45 +0200

    Updated symmetry docs.

--- docs/src/components/Figure.js
+++ docs/src/components/Figure.js
@@ -3,7 +3,7 @@ import useBaseUrl from '@docusaurus/useBaseUrl'
 
 export default function Figure({ src, caption }) {
   return (
-    <figure style={{padding: '0.5rem'}}>
+    <figure style={{padding: '0.5rem', maxWidth: "36rem", marginRight: "auto", marginLeft: "auto"}}>
       <img src={useBaseUrl(src)} alt={caption} />
       <figcaption>{`Figure: ${caption}`}</figcaption>
     </figure>
