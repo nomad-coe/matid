3cf602cd23b6ccbc1145a1fe58158a1b3fc4b975
Lauri Himanen <lauri.himanen@gmail.com>
2026-06-25 08:36:04 +0300

    Fixed url.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
   ],
 
   // Set the production url of your site here
-  url: 'https://fairmat-nfdi.github.io/matid/',
+  url: 'https://fairmat-nfdi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/matid',
